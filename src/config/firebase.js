42e9acebc0cb6ab4cb8dfeb5f8a599e48f9bb04f
agatakosior <agata.kosior@mobilereality.pl>
2020-02-04 12:30:47 +0100

    Add firebase and firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DB_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const db = firebase.firestore();
