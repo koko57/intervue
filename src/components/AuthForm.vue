a8794b792583ac4a7cc9703bc8bdf030b0e380ee
agatakosior <agata.kosior@mobilereality.pl>
2020-02-10 12:48:01 +0100

    Refactor code, add form validation for login and signup

---
+++ src/components/AuthForm.vue
@@ -0,0 +1,124 @@
+<template>
+  <form>
+    <h3>{{ form.title }}</h3>
+    <form-input
+      type="text"
+      aria-label="email"
+      v-model="email"
+      placeholder="Email"
+    />
+    <span v-if="errors.email">{{ errors.email }}</span>
+    <form-input
+      type="password"
+      aria-label="password"
+      v-model="password"
+      placeholder="Password"
+    />
+    <span v-if="errors.password">{{ errors.password }}</span>
+    <form-input
+      v-if="form.type === 'signup'"
+      type="password"
+      aria-label="confirm password"
+      v-model="confirmPassword"
+      placeholder="Confirm Password"
+    />
+    <span v-if="errors.confirmPassword">{{ errors.confirmPassword }}</span>
+    <button @click="submit">{{ form.title }}</button>
+    <span v-if="errorMessage">{{ errorMessage }}</span>
+    <p>
+      {{ form.text }}
+      <router-link v-bind:to="form.link">here</router-link>.
+    </p>
+  </form>
+</template>
+
+<script>
+import FormInput from "./FormInput";
+export default {
+  name: "AuthForm",
+  components: {
+    FormInput
+  },
+  data() {
+    return {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      errors: {
+        email: "",
+        password: "",
+        confirmPassword: ""
+      }
+    };
+  },
+  methods: {
+    submit() {
+      const valid = this.validate();
+      if (valid) {
+        const data = {
+          email: this.email,
+          password: this.password
+        };
+        this.$emit("submit", data);
+      }
+    },
+    validate() {
+      this.clearErrors();
+      if (!this.email) {
+        this.errors.email = "Email is required";
+      }
+      if (this.email && !this.matchEmailPattern(this.email)) {
+        this.errors.email = "Email is invalid";
+      }
+      if (!this.password) {
+        this.errors.password = "Password is required";
+      }
+      if (
+        this.$props.form.type === "signup" &&
+        this.password !== this.confirmPassword
+      ) {
+        this.errors.confirmPassword = "Passwords are not matching";
+      }
+      return !Object.values(this.errors).find(error => error);
+    },
+    matchEmailPattern(email) {
+      const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i;
+      return re.test(email);
+    },
+    clearErrors() {
+      this.errors.email = "";
+      this.errors.password = "";
+      this.errors.confirmPassword = "";
+    }
+  },
+  props: {
+    errorMessage: String,
+    form: Object
+  }
+};
+</script>
+
+<style scoped>
+form {
+  width: 100%;
+}
+button {
+  margin: 20px auto;
+  width: 10%;
+  cursor: pointer;
+  min-width: 100px;
+}
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+p a {
+  text-decoration: underline;
+  cursor: pointer;
+}
+span {
+  display: block;
+  color: #cd6183;
+  font-size: 0.9rem;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,69 +1,46 @@
 <template>
-  <layout>
-    <template>
-      <h3>Log In</h3>
-      <form-input
-        type="text"
-        aria-label="email"
-        v-model="email"
-        placeholder="Email"
-      />
-      <form-input
-        type="password"
-        aria-label="password"
-        v-model="password"
-        placeholder="Password"
-      />
-      <button @click="login">Log In</button>
-      <p>
-        You don't have an account ? You can create one
-        <router-link to="/sign-up">here</router-link>.
-      </p>
-    </template>
-  </layout>
+  <base-layout>
+    <auth-form
+      v-bind:form="form"
+      v-bind:errorMessage="errorMessage"
+      v-on:submit="login"
+    />
+  </base-layout>
 </template>
 
 <script>
 import { auth } from "@/config/firebase";
 import BaseLayout from "@/components/BaseLayout";
-import FormInput from "../components/FormInput";
+import AuthForm from "@/components/AuthForm";
 
 export default {
   name: "login",
   data() {
     return {
-      email: "",
-      password: ""
+      form: {
+        type: "login",
+        title: "Log In",
+        text: "You don't have an account ? You can create one",
+        link: "/sign-up"
+      },
+      errorMessage: ""
     };
   },
   components: {
-    FormInput,
-    layout: BaseLayout
+    AuthForm,
+    BaseLayout
   },
   methods: {
-    login: function() {
-      auth.signInWithEmailAndPassword(this.email, this.password).then(
+    login(data) {
+      auth.signInWithEmailAndPassword(data.email, data.password).then(
         () => {
           this.$router.replace("/");
         },
         err => {
-          alert("Something went wrong:" + err.message);
+          this.errorMessage = err.message;
         }
       );
     }
   }
 };
 </script>
-
-<style scoped>
-button {
-  margin-top: 20px;
-  width: 10%;
-  cursor: pointer;
-  min-width: 100px;
-}
-p {
-  margin-top: 40px;
-  font-size: 0.8rem;
-}
-</style>
